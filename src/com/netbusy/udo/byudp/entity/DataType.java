08074c61f96121e3ea64836a93dfdabd5d03d44f
netbusy613 <netbusy613@163.com>
2017-12-14 14:26:44 +0800

    增加了toString方法

--- src/com/netbusy/udo/byudp/entity/DataType.java
+++ src/com/netbusy/udo/byudp/entity/DataType.java
@@ -1,5 +1,19 @@
 package com.netbusy.udo.byudp.entity;
 
 public class DataType {
-    public static final int Data=0,SendOver=1,ReveiveAll=2,NeedPacket=3,Copy=4;
+    public static final int Data=0,SendOver=1,NeedPacket=2,Copy=3;
+    public static String toString(int statu){
+        switch (statu){
+            case DataType.Data:
+                return "Data";
+            case DataType.SendOver:
+                return "SendOver";
+            case DataType.NeedPacket:
+                return "NeedPacket";
+            case DataType.Copy:
+                return "Copy";
+            default:
+                return "UNKNOW";
+        }
+    }
 }
--- src/com/netbusy/udo/byudp/entity/PacketStatu.java
+++ src/com/netbusy/udo/byudp/entity/PacketStatu.java
@@ -1,5 +1,15 @@
 package com.netbusy.udo.byudp.entity;
 
 public class PacketStatu {
-    public static final int WAIT =0,OVER =1,NEED = 2,UNKNOW=3;
+    public static final int WAIT =0,OVER =1,UNKNOW=2;
+    public static String toString(int statu){
+        switch (statu){
+            case PacketStatu.WAIT:
+                return "WAIT";
+            case PacketStatu.OVER:
+                return "OVER";
+            default:
+                return "UNKNOW";
+        }
+    }
 }
